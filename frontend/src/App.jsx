3daf5824ed24ede4cdf788e5eba918e4975e2acb
Mikael L <mikaelharvard@gmail.com>
2025-11-29 16:36:32 -0500

    App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,8 @@ import Calendar from "./components/Calendar.jsx";
 function App() {
   return (
     <>
-<<<<<<< HEAD
     <Header />
     <Nav />
-=======
-      <Calendar />
->>>>>>> bd058f9649f904ae11f08d02c79187275384a8f0
     </>
   );
 }
