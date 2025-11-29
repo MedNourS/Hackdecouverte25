bd549773a741b39961fe67ddc5ddebb65f5a9215
Yaroslav K <yarokl100@gmail.com>
2025-11-29 12:37:21 -0500

    Sharing code with mikael

---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,16 @@
+//import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
+//import "/.css";
+import CalendarHeader from "./subComponents/CalendarHeader.jsx";
+
+function Calendar() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <CalendarHeader />
+    </>
+  );
+}
+
+export default Calendar;
